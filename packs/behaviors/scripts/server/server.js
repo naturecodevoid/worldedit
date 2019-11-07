bc7613e2c8be4001cc8ea4667d4214e135e1c43e
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-11-06 20:23:57 -0800

    Cleanup (not tested)
    Wooden axe support coming soon!

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -1,104 +1,148 @@
-var serverSystem = server.registerSystem(0, 0);
+const serverSystem = server.registerSystem(0, 0);
 
-var positionArray = new Array(0);
-var block, blockState, ticking_area, time = 0
+const positionArray = new Array(0);
+let block,
+    blockState,
+    tickingArea,
+    time = 0;
 
-serverSystem.initialize = function () {
-    serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => {
-        display_chat(JSON.stringify(eventData, null, '\t'))
+serverSystem.initialize = function() {
+    /*serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => {
+        displayChat(JSON.stringify(eventData, null, "\t"));
 
-        ticking_area = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.ticking_area
-        block = serverSystem.getBlock(ticking_area, eventData.data.block_position)
+        tickingArea = serverSystem.getComponent(eventData.data.player, "minecraft:tick_world").data.tickingArea;
+        block = serverSystem.getBlock(tickingArea, eventData.data.block_position);
 
-        display_chat(JSON.stringify(block, null, '\t'))
+        displayChat(JSON.stringify(block, null, "\t"));
 
-        blockState = serverSystem.getComponent(block, "minecraft:blockstate").data
+        blockState = serverSystem.getComponent(block, "minecraft:blockstate").data;
 
-        display_chat(JSON.stringify(blockState, null, '\t'))
-    })
+        displayChat(JSON.stringify(blockState, null, "\t"));
+    });*/
     serverSystem.listenForEvent("minecraft:entity_created", (eventData) => {
-        //display_chat(JSON.stringify(eventData,null,'\t'))
-        var entity = eventData.data.entity;
-        if (entity.__identifier__ === "worldedit:select") {
-            var position = serverSystem.getComponent(entity, "minecraft:position").data;
-
-            display_chat(`\nSelecting position:\nx:${position.x}\ny:${position.y}\nz:${position.z}`);
-
-            positionArray.push(position);
-            if (positionArray.length >= 3) {
-                display_chat("\nWarning:Positions exceeded.The first position is ignored.");
-                positionArray.shift();
-            }
-            serverSystem.destroyEntity(entity);
-        }
-        else if (entity.__identifier__ === "worldedit:execute") {
-            display_chat("§b NZ is JULAO!")
-
-            //display_chat(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`);
-            //serverSystem.executeCommand(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => { ; });
-            var minPosition = {
-                x: Math.min(positionArray[0].x, positionArray[1].x),
-                y: Math.min(positionArray[0].y, positionArray[1].y),
-                z: Math.min(positionArray[0].z, positionArray[1].z),
-            }
-            var maxPosition = {
-                x: Math.max(positionArray[0].x, positionArray[1].x),
-                y: Math.max(positionArray[0].y, positionArray[1].y),
-                z: Math.max(positionArray[0].z, positionArray[1].z)
-            }
-
-            display_chat(minPosition.x)
-            display_chat(minPosition.y)
-            display_chat(minPosition.z)
-            display_chat(maxPosition.x)
-            display_chat(maxPosition.y)
-            display_chat(maxPosition.z)
-
-            for (var x = minPosition.x; x <= maxPosition.x; x++) {
-                for (var y = minPosition.y; y <= maxPosition.y; y++) {
-                    for (var z = minPosition.z; z <= maxPosition.z; z++) {
-                        display_chat("Position:")
-                        display_chat(x)
-                        display_chat(y)
-                        display_chat(z)
-                        generate(x,y,z)
-                    }
-                }
-            }
-
-
-            serverSystem.destroyEntity(entity);
-        }
-    })
-}
-
-serverSystem.update = function () {
-    time++
+        //displayChat(JSON.stringify(eventData,null,'\t'))
+        const entity = eventData.data.entity;
+        if (entity.__identifier__ === "worldedit:select") entitySelect(entity);
+        else if (entity.__identifier__ === "worldedit:execute") entityExecute(entity);
+    });
+    serverSystem.listenForEvent("minecraft:entity_carried_item_changed", (eventData) =>
+        handObjectHandler(eventData.data),
+    );
 };
 
-function display_chat(message) {
-    let eventData = serverSystem.createEventData("minecraft:display_chat_event");
+serverSystem.update = function() {
+    time++;
+};
+
+function displayChat(message) {
+    const eventData = serverSystem.createEventData("minecraft:display_chat_event");
     if (eventData) {
-        eventData.data.message = message;
+        eventData.data.message = `[WorldEdit] ${message}`;
         serverSystem.broadcastEvent("minecraft:display_chat_event", eventData);
     }
 }
 
-function generate(x, y, z) {
-    serverSystem.executeCommand(`/setblock ${x} ${y} ${z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => {
+// SELECT
 
-        display_chat(JSON.stringify(commandResultData, null, '\t'));
-        display_chat("Position now:")
-        display_chat(x)
-        display_chat(y)
-        display_chat(z)
+function entitySelect(entity) {
+    const position = serverSystem.getComponent(entity, "minecraft:position").data;
 
-        var targerBlock = serverSystem.getBlock(ticking_area, x, y, z)
+    displayChat(`\nSelecting position:\nx:${position.x}\ny:${position.y}\nz:${position.z}`);
 
-        display_chat(JSON.stringify(targerBlock, null, '\t'))
+    select(position);
 
-        var targetBlockStateComponent = serverSystem.getComponent(targerBlock, "minecraft:blockstate")
-        targetBlockStateComponent.data = blockState
-        serverSystem.applyComponentChanges(targerBlock, targetBlockStateComponent)
-    });
+    serverSystem.destroyEntity(entity);
 }
+
+function axeSelect(position) {
+    select(position);
+}
+
+function select(position) {
+    positionArray.push(position);
+    if (positionArray.length >= 3) {
+        displayChat("\nWarning:Positions exceeded.The first position is ignored.");
+        positionArray.shift();
+    }
+}
+
+// EXECUTE
+
+function entityExecute(entity) {
+    execute();
+
+    serverSystem.destroyEntity(entity);
+}
+
+function playerExecute() {
+    execute();
+}
+
+function execute() {
+    //displayChat(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`);
+    //serverSystem.executeCommand(`/fill ${positionArray[0].x} ${positionArray[0].y} ${positionArray[0].z} ${positionArray[1].x} ${positionArray[1].y} ${positionArray[1].z} ${block.__identifier__.slice("minecraft:".length)}`, (commandResultData) => { ; });
+    const minPosition = {
+        x: Math.min(positionArray[0].x, positionArray[1].x),
+        y: Math.min(positionArray[0].y, positionArray[1].y),
+        z: Math.min(positionArray[0].z, positionArray[1].z),
+    };
+    const maxPosition = {
+        x: Math.max(positionArray[0].x, positionArray[1].x),
+        y: Math.max(positionArray[0].y, positionArray[1].y),
+        z: Math.max(positionArray[0].z, positionArray[1].z),
+    };
+
+    displayChat(
+        `Filling [${minPosition.x}, ${minPosition.y}, ${minPosition.z}] to [${maxPosition.x}, ${maxPosition.y}, ${
+            maxPosition.z
+        }] (${(maxPosition.x - minPosition.x) *
+            (maxPosition.y - minPosition.y) *
+            (maxPosition.z - minPosition.z)} blocks), with a block  type of ${block.__identifier__.slice(
+            "minecraft:".length,
+        )}.`,
+    );
+
+    /*displayChat(minPosition.x);
+    displayChat(minPosition.y);
+    displayChat(minPosition.z);
+    displayChat(maxPosition.x);
+    displayChat(maxPosition.y);
+    displayChat(maxPosition.z);*/
+
+    for (let x = minPosition.x; x <= maxPosition.x; x++) {
+        for (let y = minPosition.y; y <= maxPosition.y; y++) {
+            for (let z = minPosition.z; z <= maxPosition.z; z++) {
+                /*displayChat("Position:");
+                displayChat(x);
+                displayChat(y);
+                displayChat(z);*/
+                generate(x, y, z);
+            }
+        }
+    }
+}
+
+function generate(x, y, z) {
+    serverSystem.executeCommand(
+        `/setblock ${x} ${y} ${z} ${block.__identifier__.slice("minecraft:".length)}`,
+        (commandResultData) => {
+            /*displayChat(JSON.stringify(commandResultData, null, "\t"));
+            displayChat("Position now:");
+            displayChat(x);
+            displayChat(y);
+            displayChat(z);*/
+
+            const targetBlock = serverSystem.getBlock(tickingArea, x, y, z);
+
+            //displayChat(JSON.stringify(targetBlock, null, "\t"));
+
+            const targetBlockStateComponent = serverSystem.getComponent(targetBlock, "minecraft:blockstate");
+            targetBlockStateComponent.data = blockState;
+            serverSystem.applyComponentChanges(targetBlock, targetBlockStateComponent);
+        },
+    );
+}
+
+// HAND OBJECT HANDERLER
+
+function handObjectHandler(hand) {}
